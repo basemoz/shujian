35689d5faca69b23f90a66977f4e5a5d4ccacfaf
xuliangwei <liangwei.xu@gmail.com>
2011-01-15 23:04:44 +0800

    增强传送符的功能： 1、可以输入fly <全名或缩写>去各门派及主要城市。 2、可以输入fly <人名id>去此人所在的地点。

--- clone/gift/chuansong_fu.c
+++ clone/gift/chuansong_fu.c
@@ -4,6 +4,91 @@
 
 inherit ITEM;
 
+static mapping address = ([
+	"武馆":		"/d/xiangyang/damen",
+	"wg":		"/d/xiangyang/damen",
+	"wuguan":	"/d/xiangyang/damen",
+	"桃花岛":	"/d/thd/jicui",
+	"thd":		"/d/thd/jicui",
+	"taohuadao":"/d/thd/jicui",
+	"华山":		"/d/huashan/zhengqi",
+	"hs":		"/d/huashan/zhengqi",
+	"huashan":	"/d/huashan/zhengqi",
+	"少林":		"/d/shaolin/pingtai",
+	"sl":		"/d/shaolin/pingtai",
+	"shaolin":	"/d/shaolin/pingtai",
+	"星宿":		"/d/xingxiu/xxh",
+	"xx":		"/d/xingxiu/xxh",
+	"xingxiu":	"/d/xingxiu/xxh",
+	"嵩山":		"/d/songshan/damen",
+	"ss":		"/d/songshan/damen",
+	"songshan":	"/d/songshan/damen",
+	"武当":		"/d/wudang/sanqing",
+	"wd":		"/d/wudang/sanqing",
+	"wudang":	"/d/wudang/sanqing",
+	"峨嵋":		"/d/emei/houdian",
+	"em":		"/d/emei/houdian",
+	"emei":		"/d/emei/houdian",
+	"明教":		"/d/mingjiao/jyt",
+	"mj":		"/d/mingjiao/jyt",
+	"mingjiao":	"/d/mingjiao/jyt",
+	"丐帮":		"/d/gb/pomiao",
+	"gb":		"/d/gb/pomiao",
+	"gaibang":	"/d/gb/pomiao",
+	"古墓":		"/d/gumu/jqg/wshi",
+	"gm":		"/d/gumu/jqg/wshi",
+	"gumu":		"/d/gumu/jqg/wshi",
+	"慕容":		"/d/mr/yanziwu/yanziwu",
+	"mr":		"/d/mr/yanziwu/yanziwu",
+	"murong":	"/d/mr/yanziwu/yanziwu",
+	"铁掌":		"/d/tiezhang/wztang",
+	"tz":		"/d/tiezhang/wztang",
+	"tiezhang":	"/d/tiezhang/wztang",
+	"神龙岛":	"/d/sld/dukou",
+	"sld":		"/d/sld/dukou",
+	"shenlongdao":	"/d/sld/dukou",
+	"天龙寺":	"/d/dali/shanhou",
+	"tls":		"/d/dali/shanhou",
+	"tianlongsi":	"/d/dali/shanhou",
+	"昆仑":		"/d/kunlun/guangchang",
+	"kl":		"/d/kunlun/guangchang",
+	"kunlun":	"/d/kunlun/guangchang",
+	"雪山":		"/d/xueshan/guangchang",
+	"xs":		"/d/xueshan/guangchang",
+	"sueshan":	"/d/xueshan/guangchang",
+	"扬州":		"/d/city/kedian",
+	"yz":		"/d/city/kedian",
+	"yangzhou":	"/d/city/kedian",
+	"杭州":		"/d/hz/yuewangmiao",
+	"hz":		"/d/hz/yuewangmiao",
+	"hangzhou":	"/d/hz/yuewangmiao",
+	"苏州":		"/d/suzhou/beidajie1",
+	"sz":		"/d/suzhou/beidajie1",
+	"suzhou":	"/d/suzhou/beidajie1",
+	"嘉兴":		"/d/jiaxing/jiaxing",
+	"jx":		"/d/jiaxing/jiaxing",
+	"jiaxing":	"/d/jiaxing/jiaxing",
+	"长安":		"/d/changan/zhonglou",
+	"ca":		"/d/changan/zhonglou",
+	"changan":	"/d/changan/zhonglou",
+	"成都":		"/d/chengdu/center",
+	"cd":		"/d/chengdu/center",
+	"chengdu":	"/d/chengdu/center",
+	"大理":		"/d/dali/kedian",
+	"dl":		"/d/dali/kedian",
+	"dali":		"/d/dali/kedian",
+	"佛山":		"/d/foshan/guangchang",
+	"fs":		"/d/foshan/guangchang",
+	"foshan":	"/d/foshan/guangchang	",
+	"福州":		"/d/fuzhou/zhongxin",
+	"fz":		"/d/fuzhou/zhongxin",
+	"fuzhou":	"/d/fuzhou/zhongxin",
+	"attack":		"zzzzzz",
+	"attack":		"zzzzzz",
+	"attack":	"zzzzzz",
+
+]);
+
 void init()
 {
 	if (environment() == this_player()) {
@@ -28,24 +113,30 @@ void create()
 int do_fly(string arg)
 {
 	object me = this_player();
+	object ob, where;
 	string dest;
 
-	switch (arg)
+	if (!arg || arg == "")
+		return notify_fail("你想传送到哪儿?\n");
+
+	// 先判断是不是地名
+	dest = address[arg];
+
+	// 如果不是地名，判断是不是人名
+	if (!dest)
 	{
-		case "yz":
-		case "yangzhou":
-		case "扬州":
-			dest = "/d/city/kedian";
-			break;
-		case "wg":
-		case "wuguan":
-		case "武馆":
-			dest = "/d/wuguan/damen";
-			break;
-		default:
-			dest = arg;
-			break;
+		ob = find_player(arg);
+		if (!ob)
+			ob = find_living(arg);
+		if (ob)
+			where = environment(ob);
+		if (where)
+			dest = (string)file_name(where);
 	}
+
+	// 如果不是地名或人名，当作特殊地址来处理
+	if (!dest)
+		dest = arg;
 	
 	message_vision(HIG "$N将一张"
 	+name()+
